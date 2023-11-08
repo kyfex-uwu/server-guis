46cdedab3a342782c8f0403e3964f4f546c0beeb
kyfex-uwu <retrogrape117@gmail.com>
2023-11-07 23:46:15 -0700

    - each mixin method now returns early

--- src/main/java/com/kyfexuwu/server_guis/mixin/ServerEventsMixin.java
+++ src/main/java/com/kyfexuwu/server_guis/mixin/ServerEventsMixin.java
@@ -17,31 +17,34 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ServerEventsMixin {
     @Shadow public ServerPlayerEntity player;
 
-    @Inject(method="onRenameItem",at=@At("HEAD"))
+    @Inject(method="onRenameItem",at=@At("HEAD"), cancellable = true)
     public void onRenameItem__serverguis(RenameItemC2SPacket packet, CallbackInfo ci) {
         NetworkThreadUtils.forceMainThread(packet, (ServerPlayNetworkHandler)(Object)this,
                 this.player.getServerWorld());
         if (this.player.currentScreenHandler instanceof ServerGuiHandler handler) {
             handler.gui.onAnvilType(packet.getName());
+            ci.cancel();
         }
     }
 
-    @Inject(method="onUpdateBeacon",at=@At("HEAD"))
+    @Inject(method="onUpdateBeacon",at=@At("HEAD"), cancellable = true)
     public void onUpdateBeacon__serverguis(UpdateBeaconC2SPacket packet, CallbackInfo ci) {
         NetworkThreadUtils.forceMainThread(packet, (ServerPlayNetworkHandler)(Object)this,
                 this.player.getServerWorld());
         if (this.player.currentScreenHandler instanceof ServerGuiHandler handler) {
             handler.gui.onBeaconChange(packet.getPrimaryEffectId(), packet.getSecondaryEffectId());
+            ci.cancel();
         }
     }
 
-    @Inject(method="onButtonClick", at=@At("HEAD"))
+    @Inject(method="onButtonClick", at=@At("HEAD"), cancellable = true)
     //this is needed because server does a bunch of checks to see if the gui is valid
     public void onButtonClick__serverguis(ButtonClickC2SPacket packet, CallbackInfo ci) {
         NetworkThreadUtils.forceMainThread(packet, (ServerPlayNetworkHandler)(Object)this,
                 this.player.getServerWorld());
         if (this.player.currentScreenHandler instanceof ServerGuiHandler handler) {
             handler.gui.onButtonClick(packet.getButtonId());
+            ci.cancel();
         }
     }
 }
